7807b54c84f3c4dc4861643b2d6055e583a6dc7b
jahnvi09 <89133462+jahnvi09@users.noreply.github.com>
2025-11-09 19:27:50 -0500

    Update Firebase API key placeholder

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDDW64wHTn-SzJd-q6l6tlfWSwbhbbPmsY",
+    apiKey: "Insert-Your-API-Key",
     authDomain: "fir-812eb.firebaseapp.com",
     projectId: "fir-812eb",
     storageBucket: "fir-812eb.appspot.com",
